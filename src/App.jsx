daa80872d2b950b8330e7215dddd385562927246
YusufSizmaz <yusuf.sizmaz11@gmail..com>
2025-02-06 13:42:51 +0300

    Tic Tac Toe Project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { useState } from "react";
+
+import Square from "./Square";
+
+function App() {
+  return (
+    <>
+      <Square />
+    </>
+  );
+}
+
+export default App;
